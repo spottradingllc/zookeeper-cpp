958dc52c6c8f1b5cea0af7ca285126e8eae7a993
david.shrader <david.shrader@spottradingllc.com>
2014-11-04 11:53:47 -0600

    Initial commit

---
+++ sample/bin/CMakeFiles/zookeeeper-sample.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/zookeeeper-sample.dir/main.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestHarness.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestAddEphemeralNodeEventHandlerWhenNodeCreated.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestAddPersistentNodeEventHandlerWhenNodeCreated.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestAddEphemeralNodeEventHandlerWhenNodeDeleted.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestChangeEphemeralNodeAndCompare.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestChangePersistentNodeAndCompare.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestChangeSequentialEphemeralNodeAndCompare.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestChangeSequentialPersistentNodeAndCompare.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestCreateEphemeralNodeAndCompare.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestCreatePersistentNodeAndCompare.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestCreateSequentialEphemeralNodeAndCompare.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestCreateSequentialPersistentNodeAndCompare.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestGetNodeChildrenWhenNodeDoesNotExist.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestGetNodeChildrenWhenNodeExistsButNoChildren.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestGetNodeChildrenWhenChildNodeIsPersistent.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestGetNodeChildrenWhenChildNodeIsEphemeral.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestGetNodeWhenNodeDoesNotExist.cpp.o"
+  "CMakeFiles/zookeeeper-sample.dir/TestGetNodeWhenNodeExistsButDataIsEmpty.cpp.o"
+  "zookeeeper-sample.pdb"
+  "zookeeeper-sample"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/zookeeeper-sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
